5e58de38f41bbbde2b92af10e12b7b7eb97f3dda
Dione Pierre-Louis <francisd.plouis@gmail.com>
2022-09-18 03:08:56 -0600

    added js

---
+++ MS Bootcamp/Projects/SitPrep Web App/assets/js/script.js
@@ -0,0 +1,9 @@
+// var name = "";
+    
+//     while (name === "" || name === null) {
+//         name = prompt("What is your robot's name?");
+//       }
+
+//     console.log(Math.max(0, 25, -5));
+//     console.log(Math.floor(Math.random() * 60)+10);
+    
